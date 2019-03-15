5c2c3fbdfa5ffb9a1c7113feeba4ac7ed5c5dbdc
Brendan Woodell <brendan.woodell@gmail.com>
2019-03-15 11:01:23 -0700

    GOT THE SLIDER TO WORK

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,9 +1,39 @@
-import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import React, {Component} from 'react';
+// import { UncontrolledCarousel } from 'reactstrap';
+import Slider from 'react-slick'
 
-export default function Carousel(props){
-  console.log(props.items)
-  return(
-      <UncontrolledCarousel items={props.items} pause={'hover'} /> 
-    )
+// export default function Carousel(props){
+//   // console.log(props.items)
+//   // return(
+//   //     <UncontrolledCarousel items={props.items} pause={'hover'} /> 
+//   //   )
+
+
+// }
+
+export default class SimpleSlider extends Component {
+  constructor(props){
+    super()
+
+    this.state={
+      items: props.items
+    }
+  }
+
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div className='slider'>
+      <Slider {...settings}>
+        {this.state.items.map(x=> <div><img src={x.src} alt={x.altText}/></div>)}
+      </Slider>
+    </div>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import './index.css';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
